252deceb818c7be5c7b3d4e1c329240b0d81e59b
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-06-20 16:21:01 +0200

    404 completed

--- 404.php
+++ 404.php
@@ -1,5 +1,3 @@
 <?php
   get_header();
-  // get_template_part('loop');
-  // get_sidebar();
 ?>
--- footer.php
+++ footer.php
@@ -6,6 +6,5 @@
 	
     <script src="<?php echo WP_THEME_ADDR; ?>/js/vendor/jquery-1.10.1.min.js"></script>
     <script src="<?php echo WP_THEME_ADDR; ?>/js/vendor/bootstrap.min.js"></script>
-    <script src="<?php echo WP_THEME_ADDR; ?>/js/main.js"></script>
   </body>
 </html>
