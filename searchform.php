ec0fdf5437348880cce8f6cfa429ecb8d6a66c6b
cmitel <mitel.chris@gmail.com>
2014-06-20 20:20:21 +0200

    Update the style of the searchform

--- searchform.php
+++ searchform.php
@@ -1,4 +1,6 @@
-<form method="get" id="form" action="<?php bloginfo('url'); ?>/">
-  <input type="text" value="<?php the_search_query(); ?>" name="s" id="s">
-  <input type="submit" id="submit">
+<form class="form-inline" role="form" method="get" action="<?php bloginfo('url'); ?>/">
+  <div class="form-group">
+    <input type="text" class="form-control" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Your search...">
+  </div>
+  <button type="submit" class="btn btn-default">Search</button>
 </form>
