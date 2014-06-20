604048e3720e3eddac42b118bdad3dfde968dfea
cmitel <mitel.chris@gmail.com>
2014-06-20 20:26:51 +0200

    Write the search results page

---
+++ search.php
@@ -0,0 +1,14 @@
+<?php
+  get_header();
+?>
+<div class="container-fluid">
+	<div class="form">
+		<?php include_once(WP_THEME_PATH . '/searchform.php'); ?>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="posts">
+		<?php get_template_part('loop'); ?>
+	</div>
+</div>
+<?php get_footer(); ?>
