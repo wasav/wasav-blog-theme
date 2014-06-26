5b454c0079e2b268167db5e6e93ba6ae29616a66
anthonyjlmorel <anthony.jlmorel@gmail.com>
2014-06-26 17:48:13 +0200

    Copyright and some improvements

--- footer.php
+++ footer.php
@@ -4,7 +4,8 @@
   
     <?php if(function_exists('wp_footer')){ wp_footer(); } ?>
 	
-    <script src="<?php echo WP_THEME_ADDR; ?>/js/vendor/jquery-1.10.1.min.js"></script>
-    <script src="<?php echo WP_THEME_ADDR; ?>/js/vendor/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo WP_THEME_ADDR; ?>/js/vendor/jquery-1.10.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo WP_THEME_ADDR; ?>/js/vendor/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo SITE_WEB_ADDR."/js/main.js"; ?>"></script>
   </body>
 </html>
