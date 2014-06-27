39d17569e15b00112c538f21b249dd62776d5123
cmitel <mitel.chris@gmail.com>
2014-06-27 10:17:34 +0200

    Update form and style of search bar to be responsive :)

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form class="form-inline" role="form" method="get" action="<?php bloginfo('url'); ?>/">
-  <div class="form-group">
+<form class="form-inline" role="form" method="get" action="<?php bloginfo('url'); ?>/" id="searchform">
+  <div class="input-group">
     <input type="text" class="form-control" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="Your search...">
+    <span role="btn" class="input-group-addon" onclick="javascript:document.forms.searchform.submit();"><i class="glyphicon glyphicon-search"></i></span>
   </div>
-  <button type="submit" class="btn btn-default">Search</button>
 </form>
