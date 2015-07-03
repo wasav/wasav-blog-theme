8b30d9705a660c2d0b07dfe5a79439cafd70fb43
anthonyjlmorel <anthony.jlmorel@gmail.com>
2015-07-03 15:02:43 +0200

    Styel Improvement
    Added Word Wrap for small device resolutions.
    Incremented version and updated on server.

--- functions-enqueue-scripts.php
+++ functions-enqueue-scripts.php
@@ -3,10 +3,10 @@
 	function wpse_89494_enqueue_scripts() {
 		
 		// 		STYLES		
-		wp_enqueue_style( 'packedCSS', WP_THEME_ADDR."/css/style-1.3.1.min.css" );
+		wp_enqueue_style( 'packedCSS', WP_THEME_ADDR."/css/style-1.3.3.min.css" );
 		
 		// 		SCRIPTS
-		wp_enqueue_script( 'packedJs', WP_THEME_ADDR."/js/packed-1.3.1.min.js" );
+		wp_enqueue_script( 'packedJs', WP_THEME_ADDR."/js/packed-1.3.3.min.js" );
 		
 	}
 
